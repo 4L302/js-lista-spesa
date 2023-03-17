d9a77f3e9b4f5f3d8d77d7e52d173a1cfb5718a6
4L302 <alegarg2002@gmail.com>
2023-03-17 16:52:08 +0100

    .

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const spesa = [
+    'formaggio',
+    'pane',
+    'uova',
+    'caffè',
+    'zucchero'
+];
+
+
+let i = 0;
+
+while (i < spesa.length) {
+    console.log(spesa[i]);
+    i++;
+}
+
